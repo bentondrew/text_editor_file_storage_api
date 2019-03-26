fc5fdcd4ecb0aae8de30a70b26ec3811a71b87f2
bentondrew <benton.s.drew@drewantech.com>
2019-03-26 17:39:46 -0500

    Enter missing result assignment in fileRoutes.

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -7,21 +7,21 @@ const getFileRoutes = (app) => {
     const router = new Router();
     router
         .get("/:userName", (req, res) => {
-            const result fileRepo.getAllbyUser(userName);
+            const result = fileRepo.getAllbyUser(userName);
             res.send(result);
         })
         .get("/:userName/:fileName", (req, res) => {
-            const result fileRepo.getFilebyUser(userName, fileName);
+            const result = fileRepo.getFilebyUser(userName, fileName);
             res.send(result);
         })
         .post("/:userName/:fileName", (req, res) => {
             const fileContent = req.body;
-            const result fileRepo.addFilebyUser(userName, fileName, fileContent);
+            const result = fileRepo.addFilebyUser(userName, fileName, fileContent);
             res.send(result);
         })
         .put("/:userName/:fileName", (req, res) => {
             const fileContent = req.body;
-            const result fileRepo.updateFilebyUser(userName, fileName, fileContent);
+            const result = fileRepo.updateFilebyUser(userName, fileName, fileContent);
             res.send(result);
         });
     app.use("/files", router);
