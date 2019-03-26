853b70d0760169e444d3d1eb14dad80011201274
bentondrew <benton.s.drew@drewantech.com>
2019-03-26 17:48:09 -0500

    Access params from request in router.

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -7,21 +7,24 @@ const getFileRoutes = (app) => {
     const router = new Router();
     router
         .get("/:userName", (req, res) => {
-            const result = fileRepo.getAllbyUser(userName);
+            const result = fileRepo.getAllbyUser(req.params.userName);
             res.send(result);
         })
         .get("/:userName/:fileName", (req, res) => {
-            const result = fileRepo.getFilebyUser(userName, fileName);
+            const result = fileRepo.getFilebyUser(req.params.userName,
+                req.params.fileName);
             res.send(result);
         })
         .post("/:userName/:fileName", (req, res) => {
             const fileContent = req.body;
-            const result = fileRepo.addFilebyUser(userName, fileName, fileContent);
+            const result = fileRepo.addFilebyUser(req.params.userName,
+                req.params.fileName, req.params.fileContent);
             res.send(result);
         })
         .put("/:userName/:fileName", (req, res) => {
             const fileContent = req.body;
-            const result = fileRepo.updateFilebyUser(userName, fileName, fileContent);
+            const result = fileRepo.updateFilebyUser(req.params.userName,
+                req.params.fileName, req.params.fileContent);
             res.send(result);
         });
     app.use("/files", router);
